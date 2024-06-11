e3a6bc0813d711a0ec3fe8cb3d7f57987664a36a
slee2769 <slee2769@uwo.ca>
2024-06-11 14:02:48 -0400

    basic frontend done
    need to fix navigation to the portfolio page

---
+++ stocksapp/frontend/script.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+document.getElementById('view-details').addEventListener('click', function() {
+    document.getElementById('home-page').style.display = 'none';
+    document.getElementById('portfolio-page').style.display = 'block';
+});
+
+document.getElementById('return-button').addEventListener('click', function() {
+    document.getElementById('portfolio-page').style.display = 'none';
+    document.getElementById('home-page').style.display = 'block';
+});
